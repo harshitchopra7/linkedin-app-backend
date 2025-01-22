1c27392240f9ec36fb138dabfbba7117b1f8010c
harshitchopra7 <chopraharshit2000@gmail.com>
2025-01-22 14:43:31 +0530

    empty repository

--- routes/createPost.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const createPost = require("../controllers/createPostController");
-
-router.post("/", createPost);
-
-module.exports = router;
--- routes/fetchPost.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const fetchAllPosts = require("../controllers/fetchPostController");
-
-router.get("/", fetchAllPosts);
-
-module.exports = router;
